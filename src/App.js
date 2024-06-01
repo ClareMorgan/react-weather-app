b3dd65e533bdb1e8a43d3a5580ad112f67e4f88f
ClareMorgan <claredotm@gmail.com>
2024-06-01 21:55:05 +0100

    Updated styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
         <Weather />
         <Footer />
       </div>
