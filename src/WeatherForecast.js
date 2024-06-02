d4b595dd67a269f59a0f71387b0d4d93cf0f4b66
ClareMorgan <claredotm@gmail.com>
2024-06-02 23:31:52 +0100

    Updated API call to convert to metric

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,7 +28,7 @@ export default function WeatherForecast(props) {
     let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
