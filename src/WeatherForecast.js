f592a7e90620501653ca8f51095f4eaa8e4b61f6
ClareMorgan <claredotm@gmail.com>
2024-06-02 22:29:42 +0100

    Created basic structure for WeatherForecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import WeatherIcon from "./WeatherIcon";
+
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Sun</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temp-high">
+              <strong>19°</strong>
+            </span>
+            <span className="WeatherForecast-temp-low">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
