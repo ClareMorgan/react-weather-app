60c3834854a0934087f7a0baa73b9c3df39299b5
ClareMorgan <claredotm@gmail.com>
2024-06-03 19:31:57 +0100

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,6 +18,15 @@ export default function WeatherForecast(props) {
     setReady(true);
   }
 
+  function load() {
+    let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (ready) {
     return (
       <div className="WeatherForecast">
@@ -29,17 +38,16 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    load();
 
-    axios.get(apiUrl).then(handleResponse);
+    return null;
   }
 }
